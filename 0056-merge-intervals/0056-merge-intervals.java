776587a45e5f818ad94f147511bc50e802f8ae9d
ArunEshwerN <95078736+ArunEshwerN@users.noreply.github.com>
2024-06-04 21:25:43 +0530

    Time: 8 ms (86.61%), Space: 46.6 MB (32.3%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,45 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         ArrayList<int[]> al = new ArrayList<>();
 
         int n = intervals.length; 
 
-        for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
+        //Brute Force
+        // for (int i = 0; i < n; i++) {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
 
-            if (!al.isEmpty() && end <= al.get(al.size() - 1)[1]) {
-                continue;
+        //     if (!al.isEmpty() && end <= al.get(al.size() - 1)[1]) {
+        //         continue;
+        //     }
+
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (intervals[j][0] <= end) {
+        //             end = Math.max(end, intervals[j][1]);
+        //         } else {
+        //             break;
+        //         }
+        //     }
+
+        //     al.add(new int[] {start, end});
+        // }
+
+        // return al.toArray(new int[al.size()][]);
+
+        //Optimal Approach
+        for (int i = 0; i<n; i++){
+
+            if (al.isEmpty() || intervals[i][0] > al.get(al.size()-1)[1]){
+                al.add(new int[]{intervals[i][0], intervals[i][1]});
+            }
+            else{
+                al.get(al.size()-1)[1] = Math.max(intervals[i][1], al.get(al.size()-1)[1]);
             }
 
-            for (int j = i + 1; j < n; j++) {
-                if (intervals[j][0] <= end) {
-                    end = Math.max(end, intervals[j][1]);
-                } else {
-                    break;
-                }
-            }
 
-            al.add(new int[] {start, end});
         }
 
         return al.toArray(new int[al.size()][]);
