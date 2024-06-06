97c40ec415a896a04e8f2e3d53edff7483371810
ArunEshwerN <95078736+ArunEshwerN@users.noreply.github.com>
2024-06-06 11:35:55 +0530

    Time: 14 ms (16.66%), Space: 47.2 MB (88.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -32,30 +32,43 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> al = new ArrayList<>();
+        int min = ((nums.length)/3) + 1;
 
         for (int i = 0; i< nums.length; i++){
             int key = nums[i];
             int freq = 0;
+            
+
             if (map.containsKey(key)){
+
                 freq = map.get(key);
                 map.put(key, freq+1);
+
+                if ((freq+1) == min && !al.contains(key)){
+                // temp = key;
+                al.add(key);
+                }
             }
             else{
                 
                 map.put(key, freq+1);
+                if ((freq+1) == min && !al.contains(key)){
+                // temp = key;
+                al.add(key);
+                }
                 
             }
-            
+                
         }
-        int temp = 0;
-        for (Map.Entry<Integer, Integer> entry: map.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if (map.get(key)>((nums.length)/3)&&!al.contains(key)){
-                temp = key;
-                al.add(temp);
-            }
-        }
+        // int temp = 0;
+        // for (Map.Entry<Integer, Integer> entry: map.entrySet()){
+        //     int key = entry.getKey();
+        //     int value = entry.getValue();
+        //     if (map.get(key)>((nums.length)/3)&&!al.contains(key)){
+        //         temp = key;
+        //         al.add(temp);
+        //     }
+        // }
 
         return al;
     }
